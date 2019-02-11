5353ae51f8beb44ea168b99d46705f5e0683b88c
Xiaoyu Hu <hufk@bc.edu>
2019-02-11 12:50:32 -0500

    Initial Commit

---
+++ Bip the guyTests/Bip_the_guyTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bip_the_guyTests.swift
+//  Bip the guyTests
+//
+//  Created by Xiaoyu Hu on 2/11/19.
+//  Copyright © 2019 Xiaoyu Hu. All rights reserved.
+//
+
+import XCTest
+@testable import Bip_the_guy
+
+class Bip_the_guyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
